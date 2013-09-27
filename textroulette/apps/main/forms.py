053535d3af96e4de1bb0a349914d72adc2f39b6b
tlancina <tlancina@gmail.com>
2013-09-26 21:27:11 -0500

    added forms.py

---
+++ textroulette/apps/main/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from textroulette.apps.main.models import UserNumber
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = UserNumber
+        fields = ['phone_number'] 
+
+    def is_valid(self, *args, **kwargs):
+        super(UserForm).is_valid(self, *args, **kwargs)
+
