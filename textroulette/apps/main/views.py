81d2b04af2414dcc5bfa907f73fe3c91311d8960
James Pulec <jpulec@gmail.com>
2013-09-26 21:26:48 -0500

    Added connection logic

--- textroulette/apps/main/views.py
+++ textroulette/apps/main/views.py
@@ -1,10 +1,17 @@
 from django.views.generic.edit import FormView
 from textroulette.apps.main.forms import UserForm
+from textroulette.apps.main.models import UserNumber
 
 class Home(FormView):
     form_class = UserForm
     template_name = "main/home.html"
     success_url = "/success/"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        if UserNumber.objects.count() > 1:
+            self.object.connected = UserNumber.objects.exclude(id=self.object).order_by('?')[0]
+        return super(Home, self).form_valid(form)
 
 class Success(TemplateView):
     template_name = "main/success.html"
