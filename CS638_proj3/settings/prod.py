563262be74726ee251d977a8b08038fa86aa606b
James Pulec <jpulec@gmail.com>
2013-09-26 20:07:14 -0500

    Changes for easier production

--- CS638_proj3/settings/prod.py
+++ CS638_proj3/settings/prod.py
@@ -9,20 +9,20 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-AWS_QUERYSTRING_AUTH = False
+#AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+#AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+#AWS_QUERYSTRING_AUTH = False
 
 
-S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = MEDIA_URL = S3_URL
+#S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+#STATIC_URL = MEDIA_URL = #S3_URL
 
-STATIC_URL += "static/"
-MEDIA_URL += "media/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
 INSTALLED_APPS += ('gunicorn', 'storages',)
 
