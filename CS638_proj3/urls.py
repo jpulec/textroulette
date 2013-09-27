25232db80f46bdaa3f6e5e79a2ff865946cbc9da
James Pulec <jpulec@gmail.com>
2013-09-26 20:18:36 -0500

    Fixing static files

--- CS638_proj3/urls.py
+++ CS638_proj3/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-
-urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+            (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        )
